491bf5886f6585ee4d710894dcf243ab34d6983b
fireTechie <126916424+fireTechieTM@users.noreply.github.com>
2026-07-28 09:40:46 -0400

    Implement function to generate subsets of a vector

---
+++ Combinatorics/generate_subsets.cpp
@@ -0,0 +1,13 @@
+vector<vector<T>> generate_subsets(const vector<T>& a) {
+   vector<vector<T>> subsets;
+
+   for(int i = 0; i < (1 << a.size()); i++) {
+       subsets.push_back({});
+
+       for(int j = 0; (1 << j) <= i; j++) {
+           if((1 << j) & i) subsets[i].push_back(a[j]);
+       }
+   }
+
+   return subsets;
+}
